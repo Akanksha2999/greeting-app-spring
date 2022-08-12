cb13023f3d3847631edb4368489bd13066d6dd01
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-08-12 21:06:57 +0530

    ADDED: Created greeting app and displayed message

---
+++ GreetingAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingAppSpringApplication.class, args);
+    }
+
+}
